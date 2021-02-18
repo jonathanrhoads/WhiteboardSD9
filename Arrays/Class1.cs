9a4ff25d8efe10c29e79dbb62f3553446245517d
Jonathan <jonrhoads22@gmail.com>
2021-02-18 13:32:54 -0600

    Array to CSV string

---
+++ Arrays/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arrays
+{
+    public class Class1
+    {
+        //public static string TurnToCSVString(int[] arr) => string.Join(",", arr);
+
+        public static string TurnToCSVString(int[] arr)
+        {
+            string csv = "";
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                csv += arr[i] + ",";
+            }
+
+            csv += arr[arr.Length - 1];
+
+            return csv;
+        }
+    }
+}
