aa5dd87fc5a4e3e8ec2cca8b22763b9b4e546c28
Jonathan <jonrhoads22@gmail.com>
2021-02-22 15:20:55 -0600

    Adding StringEndsWith

---
+++ StringEndsWith/EndsWIth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringEndsWith
+{
+    public class EndsWIth
+    {
+        public static bool Solution(string str, string ending)
+        {
+            int i = str.Length - 1;
+            int j = ending.Length - 1;
+
+            if (j > i) return false;
+
+            while (j >= 0)
+            {
+                if (str[i] != ending[j]) return false;
+                i--;
+                j--;
+            }
+            return true;
+        }
+    }
+}
