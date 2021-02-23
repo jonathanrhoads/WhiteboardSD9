2aeac24bca51735ca360c8892f0056f78da29e97
Jonathan <jonrhoads22@gmail.com>
2021-02-23 11:02:34 -0600

    The old switcheroo

---
+++ LetterSwap/LetterSwap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LetterSwap
+{
+    public class LetterSwap
+    {
+        public static string Switcheroo(string x)
+        {
+            string s = "";
+            for (int i = 0; i < x.Length; i++)
+            {
+
+                if (x[i] == 'a') s += 'b';
+                else if (x[i] == 'b') s += 'a';
+                else s += x[i];
+            }
+            return s;
+        }
+    }
+}
