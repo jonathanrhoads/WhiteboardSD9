214a19e2da8ff98063aa24f536aa40c79e5a3278
Jonathan <jonrhoads22@gmail.com>
2021-02-24 13:50:43 -0600

    Adding rotate array

---
+++ RotateArray/Rotate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RotateArray
+{
+    public class Rotate
+    {
+        static int[] rotateArray(string dir, int dis, int[] nums)
+        {
+            if (dir == "left") dis = -dis;
+            return rotateArray(dis, nums);
+        }
+
+        static int[] rotateArray(int dis, int[] nums)
+        {
+            int[] rotated = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                int newIdx = i + dis;
+                if (newIdx > nums.Length) newIdx -= nums.Length;
+                else if (newIdx < 0) newIdx += nums.Length;
+                rotated[i] = nums[newIdx % nums.Length];
+            }
+
+            return rotated;
+        }
+    }
+}
