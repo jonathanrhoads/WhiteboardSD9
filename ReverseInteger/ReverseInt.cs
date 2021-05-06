e1d57764b27fed9cea51eb5ca09da7529bd70414
Jonathan Rhoads <jonathan.r.rhoads@outlook.com>
2021-05-06 10:37:25 -0500

    Adding reverse integer method

---
+++ ReverseInteger/ReverseInt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReverseInteger
+{
+    public class ReverseInt
+    {
+
+
+        private static double ReverseInteger(int v)
+        {
+            //to handle negatives
+            bool neg = false;
+            if (v < 0)
+            {
+                neg = true;
+                v *= -1;
+            }
+
+            double rev = 0;
+            while (v > 0)
+            {
+                rev = rev * 10 + v % 10;
+                v /= 10;
+            }
+            return neg == true ? rev * -1 : rev;
+        }
+    }
+}
